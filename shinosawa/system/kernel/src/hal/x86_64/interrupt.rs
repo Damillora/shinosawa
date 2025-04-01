b32c3bc51573d9a2b682122f2860c4bcc204d41f
Damillora <developer@damillora.com>
2025-04-02 00:02:00 +0700

    feat: convert IDT to lazy_static

--- shinosawa/system/kernel/src/hal/x86_64/interrupt.rs
+++ shinosawa/system/kernel/src/hal/x86_64/interrupt.rs
@@ -1,35 +1,24 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-
+use lazy_static::lazy_static;
 use crate::printk;
 
-// lazy_static! {
-//     static ref IDT: InterruptDescriptorTable = {
-//         let mut idt = InterruptDescriptorTable::new();
-//         idt.breakpoint.set_handler_fn(breakpoint_handler);
-//         unsafe {
-//             idt.double_fault.set_handler_fn(double_fault_handler)
-//                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
-//         }
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        printk!("x86: initializing handlers");
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
 
-//         idt
-//     };
-// }
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+        idt
+    };
+}
 
 // TODO: this is only initialized once for now
 // Find a way to dynamically load interrupt handlers?
 #[allow(static_mut_refs)]
 pub fn init() {
-    printk!("x86: initializing handlers");
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.double_fault.set_handler_fn(double_fault_handler);
-    }
-    
     printk!("x86: loading interrupts");
-    unsafe {
-        IDT.load();
-    }
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
