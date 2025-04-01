d919eac43a7505e0f4bd8621846a24e4868f233a
Damillora <developer@damillora.com>
2025-04-01 21:12:14 +0700

    chore: separate test-exclusive code

--- shinosawa/system/kernel/src/main.rs
+++ shinosawa/system/kernel/src/main.rs
@@ -1,11 +1,8 @@
 #![no_std]
 #![no_main]
-
 #![feature(custom_test_frameworks)]
 #![test_runner(tests::test_runner)]
 #![reexport_test_harness_main = "kernel_test_main"]
-
-use panic::hcf;
 
 /// Framebuffer module
 mod fb;
@@ -28,20 +25,20 @@ pub fn kernel_main() {
     let serial = unsafe { serial::init() };
 
     logger::init(display, serial);
-    
+
     {
         printk!("shinosawa::system::kernel {}", VERSION);
         printk!("an operating system for those who find joy in things that don't go well,");
         printk!("written by someone least cut out for it.");
     }
 
-    // Run tests 
     #[cfg(test)]
-    kernel_test_main();
+    {
+        use panic::hcf;
+        kernel_test_main();
 
-    // 
-    #[cfg(test)]
-    hcf();
+        hcf();
+    }
 }
 
 #[test_case]
