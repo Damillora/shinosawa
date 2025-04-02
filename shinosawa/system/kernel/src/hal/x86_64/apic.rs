5aaabe9f5ac08226a90aa45f54e7f426fe33384e
Damillora <developer@damillora.com>
2025-04-03 00:59:33 +0700

    feat: example for unleashing IO APIC

--- shinosawa/system/kernel/src/hal/x86_64/apic.rs
+++ shinosawa/system/kernel/src/hal/x86_64/apic.rs
@@ -1,11 +1,11 @@
-use acpi::InterruptModel;
+use acpi::{platform::interrupt::InterruptSourceOverride, InterruptModel};
 use conquer_once::spin::OnceCell;
 use spin::Mutex;
-use x2apic::lapic::{LocalApic, LocalApicBuilder};
+use x2apic::{ioapic::{IoApic, IrqMode, RedirectionTableEntry}, lapic::{LocalApic, LocalApicBuilder}};
 use x86_64::PhysAddr;
 
-use crate::{acpi::HARDWARE_INFO, hal::x86_64::{interrupt::InterruptIndex, paging}, printk};
-use core::ops::{Deref, DerefMut};
+use crate::{acpi::HARDWARE_INFO, hal::x86_64::{interrupt::InterruptIndex, paging::{self, map_phys_page}}, memory::{SnPhysAddr, SnVirtAddr}, printk};
+use core::{ops::{Deref, DerefMut}};
 
 // FIXME: this is not sound
 pub struct UnsafeLocalApic(pub LocalApic);
@@ -52,11 +52,35 @@ pub fn init() {
                 })
             )
         );
+
+        let mut lapic = lapic.lock();
+        
         printk!("x86_64::apic: local APIC yeeee");
         unsafe {
-            let mut lapic = lapic.lock();
             lapic.enable();
         }
+
+        let lapic_id = unsafe { lapic.id() };
+
+        printk!("x86_64::apic: unleashing IO APIC");
+        let io_apic_phys_address = apic.io_apics[0].address;
+        let io_apic_virt_address = paging::phys_to_virt_addr(PhysAddr::new(io_apic_phys_address as u64));
+        
+        map_phys_page(SnPhysAddr::new(io_apic_phys_address as u64),SnVirtAddr::new(io_apic_virt_address.as_u64()));
+        let mut io_apic = unsafe { IoApic::new(io_apic_virt_address.as_u64()) };
+
+        unsafe { io_apic.init(0) };
+
+        unsafe {
+            printk!("x86_64::apic: creating entry for IO APIC");
+            let mut entry = RedirectionTableEntry::default();
+            entry.set_mode(IrqMode::Fixed);
+            entry.set_dest(lapic_id as u8); 
+            io_apic.set_table_entry(0x01, entry);
+        
+            io_apic.enable_irq(0x01);
+        }
+
     } else {
         printk!("x86_64::apic: this system does not use APIC, apparently");
     }
