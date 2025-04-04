9a6c5af4d401580ad2fe7a97d2d2e5e86928ea09
Damillora <developer@damillora.com>
2025-04-05 03:24:16 +0700

    feat: keyboard outputs string

--- shinosawa/system/kernel/src/drivers/ps2_keyboard.rs
+++ shinosawa/system/kernel/src/drivers/ps2_keyboard.rs
@@ -1,18 +1,40 @@
-use crate::{interrupt::INTERRUPT_CONTROLLER, printk};
+use conquer_once::spin::OnceCell;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
+
+use crate::{interrupt::INTERRUPT_CONTROLLER, print, printk};
 
 const KEYBOARD_IRQ: u8 = 0x01;
 
+static KEYBOARD: OnceCell< Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>>> = OnceCell::uninit();
+
 fn keyboard_handler() {
-    
     use x86_64::instructions::port::Port;
 
+    let mut keyboard = KEYBOARD.get().unwrap().lock();
     let mut port = Port::new(0x60);
+
     let scancode: u8 = unsafe { port.read() };
-    
-    printk!("x86_64: keypress: {}", scancode);
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("#({:?})", key),
+            }
+        }
+    }
 }
 
 pub fn init() {
+    KEYBOARD.init_once(move || {
+        Mutex::new(Keyboard::new(ScancodeSet1::new(),
+            layouts::Us104Key, HandleControl::Ignore)
+        )
+    });
+
+    // Reset anything that might have been there
+    keyboard_handler();
+
     let mut controller = INTERRUPT_CONTROLLER.get().unwrap().write();
     controller.set_handler(KEYBOARD_IRQ as usize, keyboard_handler);
 
