123ef936fb2f77d0d2e239e542ac2dfe7c6f0fdc
Damillora <developer@damillora.com>
2025-04-06 21:20:01 +0700

    feat: allocate vec for loading executables instead

--- shinosawa/system/kernel/src/main.rs
+++ shinosawa/system/kernel/src/main.rs
@@ -99,11 +99,13 @@ pub fn kernel_main() {
 
     let file = crate::fs::vfs::find("SNSW:/shinosawa/system/kotono").unwrap();
     let len = file.len();
-    let mut buf = [0; 20000];
 
-    file.read(&mut buf).unwrap();
-
-    let kotono = crate::loader::elf::load_elf(&buf).unwrap();
+    let mut buf: Vec<u8> = Vec::with_capacity(len);
+    unsafe { buf.set_len(len) }; // Capacity is set in the previous line
+    file.read(buf.as_mut_slice()).unwrap();
+    
+    printk!("{}", buf.len());
+    let kotono = crate::loader::elf::load_elf(buf.as_slice()).unwrap();
 
     // We can *actually* start a user process now.
     crate::process::thread::new_user_thread(kotono);
